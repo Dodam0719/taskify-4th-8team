f34eee34721e31a2114a9c40e727ad4f908e6f18
gaem-gaem <kkmm080399@gmail.com>
2024-04-16 16:24:11 +0900

    Comment: 카드 태그 주석 추가

--- components/chips/card-tag-chips/CardTagChips.tsx
+++ components/chips/card-tag-chips/CardTagChips.tsx
@@ -5,6 +5,7 @@ const CARD_TAG_NAME = ['프로젝트', '일반', '백엔드', '상'];
 const CARD_TAG_COLOR = ['#F9EEE3', '#E7F7DB', '#F7DBF0', '#DBE6F7'];
 const CARD_TAG_TEXT_COLOR = ['#D58D49', '#86D549', '#D549B6', '#4981D5'];
 
+// 사용자 입력값으로 칩 내용 수정 예정
 const CardTagChips = () => {
   return (
     <S.CardTagChipsStyle>
