e71e793b68f18d59eaab2dfe176e2de91514acad
Dodam0719 <blueoh719@gmail.com>
2024-04-16 16:06:28 +0900

    Refactor: ModalAlert 모달 바깥영역 클릭시 모달창 닫히도록 수정

--- components/Modal/ModalAlert.tsx
+++ components/Modal/ModalAlert.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import * as S from './ModalAlert.style';
 import ModalButton from './ModalButton';
 
@@ -7,9 +8,26 @@ interface ModalAlertProps {
 }
 
 const ModalAlert: React.FC<ModalAlertProps> = ({ message, onClose }) => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      // ref.current는 참조 객체의 현재 값(외부 클릭을 감지하고자 하는 대상)
+      // ref.current가 event.target을 포함하는지 판단하여 !연산
+      // 즉, event.target이 외부에서 발생했다면 드롭다운 메뉴를 false하여 닫음
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target as Node)) {
+        onClose();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
   return (
     <S.ModalOverlay>
-      <S.ModalWrapper>
+      <S.ModalWrapper ref={wrapperRef}>
         <S.ModalAlertContent>
           <p>{message}</p>
         </S.ModalAlertContent>
