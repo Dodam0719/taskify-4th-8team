1cbbc2dd0df6a37b2390fa3113265549201167b8
gaem-gaem <kkmm080399@gmail.com>
2024-04-20 03:24:51 +0900

    Refactor: PlusChip 스타일드 컴포넌트 수정

--- components/chips/plus-chip/PlusChip.tsx
+++ components/chips/plus-chip/PlusChip.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image';
 import * as S from './PlusChip.style';
 
 const PlusChip = () => {
   return (
-    <S.PlusChipStyle>
-      <Image className='add-image' src='/assets/chips/add-chips.svg' alt='추가하기 이미지' width={16} height={16} />
-    </S.PlusChipStyle>
+    <S.PlusChip>
+      <S.AddImage src='/assets/chips/add-chips.svg' alt='추가하기 이미지' width={16} height={16} />
+    </S.PlusChip>
   );
 };
 
