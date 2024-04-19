23587705aec1b8dea06059eb4250848cd64bb55e
gaem-gaem <kkmm080399@gmail.com>
2024-04-20 04:03:04 +0900

    Refactor: SideMenu 스타일드 컴포넌트 수정

--- components/side-menu/SideMenu.tsx
+++ components/side-menu/SideMenu.tsx
@@ -7,21 +7,21 @@ const MENU_NAME = ['비브리지', '코드잇', '3분기 계획', '회의록', '
 
 const SideMenu = () => {
   return (
-    <S.SideMenuStyle>
-      <section className='logo-container'>
-        <Image className='logo-image' src='/assets/sidemenu/logo-image.svg' alt='로고 이미지' width={29} height={33} />
-        <Image className='logo-title' src='/assets/sidemenu/logo-title.svg' alt='로고 텍스트' width={80} height={22} />
-      </section>
-      <section className='container'>
-        <span className='font-12-bold text'>Dash Boards</span>
+    <S.SideMenu>
+      <S.LogoContainer>
+        <S.LogoImage src='/assets/sidemenu/logo-image.svg' alt='로고 이미지' width={29} height={33} />
+        <S.LogoTitle src='/assets/sidemenu/logo-title.svg' alt='로고 텍스트' width={80} height={22} />
+      </S.LogoContainer>
+      <S.Container>
+        <S.Text>Dash Boards</S.Text>
         <Image src='/assets/icon/add-icon.svg' alt='추가 아이콘' width={20} height={20} />
-      </section>
-      <section className='list-container'>
+      </S.Container>
+      <S.ListContainer>
         {COLOR.map((color, index) => (
           <Menu key={index} color={color} menuName={MENU_NAME[index]} />
         ))}
-      </section>
-    </S.SideMenuStyle>
+      </S.ListContainer>
+    </S.SideMenu>
   );
 };
 
