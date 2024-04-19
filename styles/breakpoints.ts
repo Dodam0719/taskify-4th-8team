be5f5f4bf37b6b34fe93151bbe7c2477bf494cca
gaem-gaem <kkmm080399@gmail.com>
2024-04-20 02:40:46 +0900

    Feat: breakpoint tablet min-width 추가

--- styles/breakpoints.ts
+++ styles/breakpoints.ts
@@ -1,10 +1,12 @@
 const size = {
   mobile: 767,
+  tabletMin: 768,
   tablet: 1280,
 };
 
 const device = {
   mobile: `(max-width: ${size.mobile}px)`,
+  tabletMin: `(min-width: ${size.tabletMin}px)`,
   tablet: `(max-width: ${size.tablet}px)`,
 };
 
