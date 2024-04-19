55fb769b9fd4fec6c49bca9faa351d82642dfdff
gaem-gaem <kkmm080399@gmail.com>
2024-04-20 03:04:05 +0900

    Refactor: 카드 리스트 필요없는 스타일 제거

--- components/card/CardList.style.ts
+++ components/card/CardList.style.ts
@@ -1,25 +1,6 @@
 import { device } from '@/styles/breakpoints';
 import styled from 'styled-components';
 
-export const CardListStyle = styled.div`
-  padding: 2rem;
-  border-right: 1px solid var(--gray_080);
-
-  .card-container {
-    display: flex;
-    flex-direction: column;
-    gap: 1.6rem;
-  }
-
-  .settings-icon {
-    cursor: pointer;
-  }
-
-  @media screen and (max-width: 1024px) {
-    border-right: none;
-  }
-`;
-
 export const CardList = styled.div`
   padding: 2rem;
   border-right: 1px solid var(--gray_080);
