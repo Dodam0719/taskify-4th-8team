7d17ee40076d1bf5f17a94cd0a0edb2ca7cfc04c
gaem-gaem <kkmm080399@gmail.com>
2024-04-19 23:32:51 +0900

    Design: 초대받은 대시보드 리스트 모바일 구현

---
+++ components/table/invitedash/InviteItemMobile.tsx
@@ -0,0 +1,29 @@
+import * as S from './InviteItemMobile.style';
+
+interface InviteItemType {
+  name: string;
+  inviter: string;
+}
+
+const InviteItemMobile = ({ name, inviter }: InviteItemType) => {
+  return (
+    <S.InviteItemMobileStyle>
+      <S.NameLabelStyle>
+        <S.ContainerStyle>
+          <S.LabelStyle>이름</S.LabelStyle>
+          <span>{name}</span>
+        </S.ContainerStyle>
+        <S.ContainerStyle>
+          <S.LabelStyle>초대자</S.LabelStyle>
+          <span>{inviter}</span>
+        </S.ContainerStyle>
+      </S.NameLabelStyle>
+      <S.ButtonContainerStyle>
+        <S.ButtonStyle>수락</S.ButtonStyle>
+        <S.ButtonStyle>거절</S.ButtonStyle>
+      </S.ButtonContainerStyle>
+    </S.InviteItemMobileStyle>
+  );
+};
+
+export default InviteItemMobile;
