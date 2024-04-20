5880d0da883e37a8d793239de042a5f04326c1f4
gaem-gaem <kkmm080399@gmail.com>
2024-04-20 14:17:18 +0900

    Refactor: 필요없는 코드 제거

--- components/table/table-header/TableHeader.style.ts
+++ components/table/table-header/TableHeader.style.ts
@@ -2,92 +2,6 @@ import { device } from '@/styles/breakpoints';
 import Image from 'next/image';
 import styled from 'styled-components';
 
-export const TableHeaderStyle = styled.section`
-  margin-bottom: 2.4rem;
-
-  .button {
-    width: 10.6rem;
-    height: 3.2rem;
-  }
-
-  .title-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 2.7rem;
-
-    .pagination-container {
-      display: flex;
-      align-items: center;
-      gap: 1.6rem;
-    }
-
-    .arrow {
-      padding: 1.2rem;
-      border-radius: 0 0.4rem 0.4rem 0;
-      border: 1px solid var(--gray_060);
-      background: var(--white_100);
-    }
-
-    .arrow-back-page {
-      transform: scaleX(-1);
-    }
-  }
-
-  .list-title-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    .list-name {
-      color: var(--gray_020);
-    }
-    .button {
-      display: none;
-    }
-  }
-
-  @media screen and (max-width: 767px) {
-    margin-bottom: 2rem;
-
-    .title-container {
-      margin-bottom: 1.2rem;
-    }
-
-    .title {
-      font-size: 2rem;
-    }
-
-    .pagination-container {
-      display: flex;
-      align-items: center;
-      gap: 1.2rem;
-
-      .page-number {
-        font-size: 1.2rem;
-      }
-    }
-
-    .arrow {
-      width: 3.6rem;
-      height: 3.6rem;
-      padding: 1rem;
-    }
-
-    .pagination-container > .button {
-      display: none;
-    }
-
-    .list-title-container > .button {
-      display: block;
-    }
-  }
-`;
-
-// ----------------------------------------------------
-// ----------------------------------------------------
-// ----------------------------------------------------
-
 export const TableHeader = styled.article`
   margin-bottom: 2.4rem;
 
@@ -190,6 +104,4 @@ export const ListName = styled.p`
   color: var(--gray_020);
   font-size: 1.6rem;
   font-weight: 400;
-  @media ${device.mobile} {
-  }
 `;
