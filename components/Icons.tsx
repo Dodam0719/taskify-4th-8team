dcee2158d26ee750c1124a1bad88cd846d4614e2
gaem-gaem <kkmm080399@gmail.com>
2024-04-21 00:27:17 +0900

    Design, Refactor: Icon 컴포넌트 반응형 추가, 오타 수정

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,3 +1,4 @@
+import { device } from '@/styles/breakpoints';
 import styled, { css } from 'styled-components';
 
 export const Add = styled.div`
@@ -72,10 +73,21 @@ export const Kebab = styled.div`
   height: 2.8rem;
 `;
 
-export const Serach = styled.div`
+export const Search = styled.div`
   background: url('/icons/Search.svg') no-repeat;
+  position: absolute;
+  top: 0.7rem;
+  left: 1.6rem;
+
   width: 2.4rem;
   height: 2.4rem;
+
+  @media ${device.mobile} {
+    width: 2.2rem;
+    height: 2.2rem;
+    top: 0.7rem;
+    left: 1.2rem;
+  }
 `;
 
 export const Setting = styled.div`
@@ -84,7 +96,7 @@ export const Setting = styled.div`
   height: 2rem;
 `;
 
-export const Unsubcribe = styled.div`
+export const Unsubscribe = styled.div`
   background: url('/icons/Unsubscribe.svg') no-repeat;
   width: 10rem;
   height: 10rem;
