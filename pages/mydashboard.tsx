a8ebd5b51b9bbc103419bdf72ae66e13f3912819
gaem-gaem <kkmm080399@gmail.com>
2024-04-23 01:53:59 +0900

    Design: /mydashboard 수정

--- pages/mydashboard.tsx
+++ pages/mydashboard.tsx
@@ -2,13 +2,17 @@ import SideMenu from '@/components/side-menu/SideMenu';
 import * as S from './mydashboard.style';
 import NoInvitedDashboard from '@/components/table/no-invited-dashboard/NoInvitedDashboard';
 import InviteDash from '@/components/table/invitedash/InviteDash';
+import DashboardHeader from '@/components/DashboardHeader/DashboardHeader';
 
 const MyDashBoard = () => {
   return (
     <S.MyDashBoardStyle>
       <SideMenu />
-      {/* <NoInvitedDashboard /> */}
-      <InviteDash />
+      <S.ContainerStyle>
+        {/* <DashboardHeader /> */}
+        {/* <NoInvitedDashboard /> */}
+        <InviteDash />
+      </S.ContainerStyle>
     </S.MyDashBoardStyle>
   );
 };
