0dd7f96c1f98f3092b9dd15d28ee49377dacd5bc
gaem-gaem <kkmm080399@gmail.com>
2024-04-23 17:09:06 +0900

    Design: SideMenu 왕관 위치 변경, 글자 넘칠 떄 UI 수정

--- components/side-menu/Menu.tsx
+++ components/side-menu/Menu.tsx
@@ -6,10 +6,12 @@ const Menu = ({ color, menuName }: MenuType) => {
     <S.Menu>
       <S.MenuContainer>
         <S.Point color={color} />
-        <S.MenuItem>{menuName}</S.MenuItem>
+        <S.MenuItemContainerStyle>
+          {/* 내가 만든 대시보드에만 달리게 조건 달아줘야 함 */}
+          <S.CrownIcon src='/icons/Crown.svg' alt='왕관 아이콘' width={18} height={14} />
+          <S.MenuItem>{menuName}</S.MenuItem>
+        </S.MenuItemContainerStyle>
       </S.MenuContainer>
-      {/* 내가 만든 대시보드에만 달리게 조건 달아줘야 함 */}
-      <S.CrownIcon src='/icons/Crown.svg' alt='왕관 아이콘' width={18} height={14} />
     </S.Menu>
   );
 };
