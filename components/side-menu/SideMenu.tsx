0d2b60a7c85ae933bd6e78c53456c6a79c29e579
Dodam0719 <blueoh719@gmail.com>
2024-04-24 23:53:18 +0900

    feat: SideMenu에 ModalNewdash 연결해서 POST 기능까지 완료

--- components/side-menu/SideMenu.tsx
+++ components/side-menu/SideMenu.tsx
@@ -2,12 +2,18 @@ import * as S from './SideMenu.style';
 import Menu from './Menu';
 import { Add } from '../Icons';
 import useDashboards from '@/hooks/useDashboards';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import ModalNewdash from '../Modal/ModalNewdash';
 
 const COLOR = ['--green_100', '--purple_100', '--orange_100', '--blue_100', '--pink_100', '--green_100'];
 const MENU_NAME = ['비브리지', '코드잇', '3분기 계획', '회의록', '중요 문서함', '가나다라마바아'];
 
 const SideMenu = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
   const { dashboards, addDashboard, loadDashboards } = useDashboards();
 
   useEffect(() => {
@@ -26,7 +32,7 @@ const SideMenu = () => {
       </S.LogoContainer>
       <S.Container>
         <S.Text>Dash Boards</S.Text>
-        <Add $width='2rem' $height='2rem' />
+        <Add $width='2rem' $height='2rem' onClick={handleOpenModal} />
       </S.Container>
       <S.ListContainer>
         {myDashboards.map((dashboard) => (
@@ -36,6 +42,7 @@ const SideMenu = () => {
           <Menu key={dashboard.id} dashboard={dashboard} />
         ))}
       </S.ListContainer>
+      {isModalOpen && <ModalNewdash dashboards={dashboards} onSubmit={addDashboard} onClose={handleCloseModal} />}
     </S.SideMenu>
   );
 };
