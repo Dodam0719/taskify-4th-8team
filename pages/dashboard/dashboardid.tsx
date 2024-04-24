84c5aa541ebea811b2cfed0e5114a4c28ddce6ba
gaem-gaem <kkmm080399@gmail.com>
2024-04-24 16:59:22 +0900

    Feat: 카드정보 더미데이터에서 받아오기

--- pages/dashboard/dashboardid.tsx
+++ pages/dashboard/dashboardid.tsx
@@ -4,17 +4,33 @@ import Card from '@/components/card/Card';
 import Button from '@/components/button/Button';
 import PlusChip from '@/components/chips/plus-chip/PlusChip';
 import DashboardHeader from '@/components/DashboardHeader/DashboardHeader';
+import { dummyCards } from '../api/dummyCard';
+import { CardType } from '../api/dummyCardDataType';
+
+const columnIdToTitleMap: { [key: string]: string } = {
+  '0': 'To do',
+  '1': 'On Progress',
+  '2': 'Done',
+};
 
 const Dashboard = () => {
+  const cardLists: { [key: string]: CardType[] } = {};
+
+  Object.keys(columnIdToTitleMap).forEach((columnId) => {
+    cardLists[columnId] = dummyCards[0].cards.filter((card) => card.columnId === parseInt(columnId));
+  });
+
   return (
     <S.DashboardStyle>
       <SideMenu />
       <S.WrapperStyle>
         <DashboardHeader isShow='show' />
         <S.CardListStyle>
-          <Card>To do</Card>
-          <Card>On Progress</Card>
-          <Card>Done</Card>
+          {Object.entries(cardLists).map(([columnId, cardList]) => (
+            <Card key={columnId} cardList={cardList}>
+              {columnIdToTitleMap[columnId]}
+            </Card>
+          ))}
           <S.ButtonStyle>
             <Button variant='addColumn' $width='35.4rem' $height='7rem'>
               <span>새로운 컬럼 추가하기</span>
