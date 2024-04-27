a8cfb0d1321e7ee3056b6f2b836ab8a6e2138023
gaem-gaem <kkmm080399@gmail.com>
2024-04-28 03:39:02 +0900

    Feat: 초대받은 대시보드 API 연결

--- components/table/invitedash/InviteDash.tsx
+++ components/table/invitedash/InviteDash.tsx
@@ -1,14 +1,16 @@
-import { Arrow_forward, Arrow_forward_disabled, Search } from '@/components/Icons';
+import { Arrow_forward, Search } from '@/components/Icons';
 import * as S from './InviteDash.style';
 import InviteItem from './InviteItem';
 import InviteItemMobile from './InviteItemMobile';
 import Button from '@/components/button/Button';
 import PlusChip from '@/components/chips/plus-chip/PlusChip';
-import useDashboards from '@/hooks/useDashboards';
 import { useEffect, useState } from 'react';
 import Menu from '@/components/side-menu/Menu';
 import ModalNewdash from '@/components/Modal/ModalNewdash';
 import useGetDashboards from '@/query/useGetDashboards';
+import { apiCall } from '@/pages/api/api';
+import Noinvited from '../no-invited-dashboard/NoInvited';
+import Link from 'next/link';
 
 const INVITE_ITEM = [
   ['프로덕트 디자인', '손동희'],
@@ -21,11 +23,21 @@ const INVITE_ITEM = [
 
 const InviteDash = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedDashboardIndex, setSelectedDashboardIndex] = useState(-1);
+  const [inviteData, setInviteData] = useState();
 
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
 
   const { data, isPending, isError } = useGetDashboards();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await apiCall('get', 'https://sp-taskify-api.vercel.app/4-8/invitations?size=10');
+      setInviteData(data);
+    };
+    fetch();
+  }, []);
 
   if (isPending) return <div>로딩중</div>;
   if (isError) return <div>에러</div>;
@@ -43,16 +55,42 @@ const InviteDash = () => {
           <PlusChip />
         </Button>
         {myDashboards.map((dashboard) => (
-          <Button variant='dashboard' $width='33.2rem' $height='7rem'>
-            <div>
-              <S.ButtonColorPointStyle color={dashboard.color} />
-              <Menu key={dashboard.id} dashboard={dashboard} />
-            </div>
-            <Arrow_forward $width='1.8rem' $height='1.8rem' />
-          </Button>
+          <Link href={`/dashboard/${dashboard.id}`} key={dashboard.id}>
+            <Button variant='dashboard' $width='33.2rem' $height='7rem'>
+              <div>
+                <S.ButtonColorPointStyle color={dashboard.color} />
+                <Menu
+                  id={dashboard.id}
+                  dashboard={dashboard}
+                  selectedDashboardIndex={selectedDashboardIndex}
+                  setSelectedDashboardIndex={(index) => {
+                    setSelectedDashboardIndex(index);
+                  }}
+                  type='button'
+                />
+              </div>
+              <Arrow_forward $width='1.8rem' $height='1.8rem' />
+            </Button>
+          </Link>
         ))}
         {inviteDashBoards.map((dashboard) => (
-          <Menu key={dashboard.id} dashboard={dashboard} />
+          <Link href={`/dashboard/${dashboard.id}`} key={dashboard.id}>
+            <Button variant='dashboard' $width='33.2rem' $height='7rem'>
+              <div>
+                <S.ButtonColorPointStyle color={dashboard.color} />
+                <Menu
+                  id={dashboard.id}
+                  dashboard={dashboard}
+                  selectedDashboardIndex={selectedDashboardIndex}
+                  setSelectedDashboardIndex={(index) => {
+                    setSelectedDashboardIndex(index);
+                  }}
+                  type='button'
+                />
+              </div>
+              <Arrow_forward $width='1.8rem' $height='1.8rem' />
+            </Button>
+          </Link>
         ))}
       </S.ButtonContainerStyle>
       <S.PageNationContainer>
@@ -62,30 +100,46 @@ const InviteDash = () => {
           <S.ArrowNextPage src='/icons/Arrow_forward.svg' alt='다음 페이지로' width={40} height={40} />
         </S.ArrowContainer>
       </S.PageNationContainer>
-      <S.InviteDashListStyle>
-        <S.InviteDashContainerStyle>
-          <S.TitleStyle>초대받은 대시보드</S.TitleStyle>
-          <S.SearchFormStyle>
-            <S.SearchInputStyle placeholder='검색' />
-            <Search />
-          </S.SearchFormStyle>
-        </S.InviteDashContainerStyle>
-        <S.ListHeaderStyle>
-          <S.ListHeaderItemStyle>이름</S.ListHeaderItemStyle>
-          <S.ListHeaderItemStyle>초대자</S.ListHeaderItemStyle>
-          <S.ListHeaderItemStyle>수락 여부</S.ListHeaderItemStyle>
-        </S.ListHeaderStyle>
-        <S.ListStyle>
-          {INVITE_ITEM.map((item, i) => (
-            <InviteItem key={i} name={item[0]} inviter={item[1]} />
-          ))}
-        </S.ListStyle>
-        <S.ListMobileStyle>
-          {INVITE_ITEM.map((item, i) => (
-            <InviteItemMobile key={i} name={item[0]} inviter={item[1]} />
-          ))}
-        </S.ListMobileStyle>
-      </S.InviteDashListStyle>
+      {inviteData?.invitations.length === 0 ? (
+        <Noinvited />
+      ) : (
+        <S.InviteDashListStyle>
+          <S.InviteDashContainerStyle>
+            <S.TitleStyle>초대받은 대시보드</S.TitleStyle>
+            <S.SearchFormStyle>
+              <S.SearchInputStyle placeholder='검색' />
+              <Search />
+            </S.SearchFormStyle>
+          </S.InviteDashContainerStyle>
+          <S.ListHeaderStyle>
+            <S.ListHeaderItemStyle>이름</S.ListHeaderItemStyle>
+            <S.ListHeaderItemStyle>초대자</S.ListHeaderItemStyle>
+            <S.ListHeaderItemStyle>수락 여부</S.ListHeaderItemStyle>
+          </S.ListHeaderStyle>
+          <S.ListStyle>
+            {inviteData?.invitations.map((item) => (
+              <InviteItem
+                key={item.id}
+                id={item.id}
+                name={item.dashboard.title}
+                inviter={item.inviter.nickname}
+                setInviteData={setInviteData}
+              />
+            ))}
+          </S.ListStyle>
+          <S.ListMobileStyle>
+            {inviteData?.invitations.map((item) => (
+              <InviteItemMobile
+                key={item.id}
+                id={item.id}
+                name={item.dashboard.title}
+                inviter={item.inviter.nickname}
+                setInviteData={setInviteData}
+              />
+            ))}
+          </S.ListMobileStyle>
+        </S.InviteDashListStyle>
+      )}
       {isModalOpen && <ModalNewdash dashboards={dashboards} onClose={handleCloseModal} />}
     </S.InviteDashStyle>
   );
